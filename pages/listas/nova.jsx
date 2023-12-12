6ee7da982a5cc722bc4976b47110ee7623685ae2
tgo-mas <talmeidasf@gmail.com>
2023-12-12 15:21:42 -0300

    Ajustes nas listas e redirecionamento no login

---
+++ pages/listas/nova.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+
+export default function NovaLista(){
+
+    return(
+        <Container>
+            <h1>Nova lista</h1>
+        </Container>
+    );
+
+}
